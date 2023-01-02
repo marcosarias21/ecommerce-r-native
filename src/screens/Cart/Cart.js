a97a6f4d7948590cf879866ba8f2fc735a1b4010
marcosarias21 <marcosariasolaiz@gmail.com>
2023-01-02 16:52:21 -0300

    add text to show total price to pay

--- src/screens/Cart/Cart.js
+++ src/screens/Cart/Cart.js
@@ -1,15 +1,27 @@
-import { ScrollView } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ScrollView, View } from 'react-native';
+import { Text } from 'react-native-paper';
 import { useSelector } from 'react-redux';
+import { styles } from '../../../AppStyles';
 import { CartProduct } from '../../components/CartProduct';
 
 const Cart = () => {
+  const [totalPay, setTotalPay] = useState([]);
   const { items } = useSelector((state => state.products));
-  console.log(items);
+  useEffect(() => {
+    setTotalPay(items.reduce((previousValue, currentValue) => {
+      return previousValue + currentValue.price;
+    }, 0));
+    console.log(totalPay);
+  }, [items]);
   return (
-   <ScrollView>
+   <ScrollView style={styles.container}>
     {
       items?.map((product, index) => <CartProduct {...product} key={product.id} index={index} />)
     }
+    <View>
+      <Text variant='headlineMedium'>Total to pay: ${totalPay} </Text>
+    </View>
    </ScrollView>
   );
 };
